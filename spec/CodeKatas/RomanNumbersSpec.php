2f09389894516cef66ae1ca5649542146440c84b
leinbg <leiw4github@gmail.com>
2016-09-23 23:28:10 +0200

    romanNumbers done

--- spec/CodeKatas/RomanNumbersSpec.php
+++ spec/CodeKatas/RomanNumbersSpec.php
@@ -14,11 +14,69 @@ use Prophecy\Argument;
 class RomanNumbersSpec extends ObjectBehavior
 {
 
-    /**
-     * init test
-     */
     function it_is_initializable()
     {
         $this->shouldHaveType(RomanNumbers::class);
     }
+
+    function it_throws_exception_when_argument_not_integer()
+    {
+        $this->shouldThrow(new \InvalidArgumentException('Not an Integer'))->during('convert', ['a']);
+    }
+
+    function it_convert_1_to_I()
+    {
+        $this->convert(1)->shouldBe('I');
+    }
+
+    function it_convert_2_to_II()
+    {
+        $this->convert(2)->shouldBe('II');
+    }
+
+    function it_convert_4_to_IV()
+    {
+        $this->convert(4)->shouldBe('IV');
+    }
+
+    function it_convert_5_to_V()
+    {
+        $this->convert(5)->shouldBe('V');
+    }
+
+    function it_convert_6_to_VI()
+    {
+        $this->convert(6)->shouldBe('VI');
+    }
+
+    function it_convert_9_to_IX()
+    {
+        $this->convert(9)->shouldBe('IX');
+    }
+
+    function it_convert_10_to_X()
+    {
+        $this->convert(10)->shouldBe('X');
+    }
+
+    function it_convert_18_to_XVIII()
+    {
+        $this->convert(18)->shouldBe('XVIII');
+    }
+
+    function it_convert_37_to_XXXVII()
+    {
+        $this->convert(37)->shouldBe('XXXVII');
+    }
+
+    function it_convert_48_to_XLVIII()
+    {
+        $this->convert(48)->shouldBe('XLVIII');
+    }
+
+    function it_convert_99_to_XCIX()
+    {
+        $this->convert(99)->shouldBe('XCIX');
+    }
 }
+
--- src/CodeKatas/RomanNumbers.php
+++ src/CodeKatas/RomanNumbers.php
@@ -9,4 +9,39 @@ namespace CodeKatas;
  */
 class RomanNumbers
 {
+
+    protected $units = [
+        'C' => '100',
+        'XC' => '90',
+        'L' => '50',
+        'XL' => '40',
+        'X' => '10',
+        'IX' => '9',
+        'V' => '5',
+        'IV' => '4',
+        'I' => '1',
+    ];
+
+    /**
+     * @param int $integer int
+     *
+     * @return string
+     */
+    public function convert($integer)
+    {
+        if (is_int($integer) == false) {
+            throw new \InvalidArgumentException('Not an Integer');
+        }
+
+        $romanNumber = '';
+
+        foreach ($this->units as $unit => $number) {
+            while ($integer >= $number) {
+                $romanNumber .= $unit;
+                $integer -= $number;
+            }
+        }
+
+        return $romanNumber;
+    }
 }
