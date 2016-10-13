e9ad2328a5713a87dff1d0ede98871d3330f5dc9
leinbg <leiw4github@gmail.com>
2016-10-13 22:08:35 +0200

    make test function public

--- spec/CodeKatas/NumbersInWordsSpec.php
+++ spec/CodeKatas/NumbersInWordsSpec.php
@@ -14,36 +14,36 @@ use Prophecy\Argument;
 class NumbersInWordsSpec extends ObjectBehavior
 {
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(NumbersInWords::class);
     }
 
-    function it_converts_integer_less_than_10()
+    public function it_converts_integer_less_than_10()
     {
         $this->convert(1)->shouldBe('yi');
         $this->convert(5)->shouldBe('wu');
     }
 
-    function it_converts_integer_less_than_100()
+    public function it_converts_integer_less_than_100()
     {
         $this->convert(43)->shouldBe('si shi san');
         $this->convert(14)->shouldBe('shi si');
     }
 
-    function it_converts_integer_less_than_1000()
+    public function it_converts_integer_less_than_1000()
     {
         $this->convert(198)->shouldBe('yi bai jiu shi ba');
         $this->convert(423)->shouldBe('si bai er shi san');
     }
 
-    function it_converts_integer_less_than_10000()
+    public function it_converts_integer_less_than_10000()
     {
         $this->convert(1111)->shouldBe('yi qian yi bai yi shi yi');
         $this->convert(3456)->shouldBe('san qian si bai wu shi liu');
     }
 
-    function it_converts_float_more_than_1()
+    public function it_converts_float_more_than_1()
     {
         $this->convert(1.23)->shouldBe('yi dian er san');
         $this->convert(23.01)->shouldBe('er shi san dian ling yi');
