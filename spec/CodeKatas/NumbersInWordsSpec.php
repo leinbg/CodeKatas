d8bce140f7f88ff73bf5054d916cb7c2f57bff93
leinbg <leiw4github@gmail.com>
2016-10-13 22:06:35 +0200

    rename test function

--- spec/CodeKatas/NumbersInWordsSpec.php
+++ spec/CodeKatas/NumbersInWordsSpec.php
@@ -19,25 +19,25 @@ class NumbersInWordsSpec extends ObjectBehavior
         $this->shouldHaveType(NumbersInWords::class);
     }
 
-    function it_converts_one_digit_integer()
+    function it_converts_integer_less_than_10()
     {
         $this->convert(1)->shouldBe('yi');
         $this->convert(5)->shouldBe('wu');
     }
 
-    function it_converts_integer_under_100()
+    function it_converts_integer_less_than_100()
     {
         $this->convert(43)->shouldBe('si shi san');
         $this->convert(14)->shouldBe('shi si');
     }
 
-    function it_converts_integer_under_1000()
+    function it_converts_integer_less_than_1000()
     {
         $this->convert(198)->shouldBe('yi bai jiu shi ba');
         $this->convert(423)->shouldBe('si bai er shi san');
     }
 
-    function it_converts_integer_under_10000()
+    function it_converts_integer_less_than_10000()
     {
         $this->convert(1111)->shouldBe('yi qian yi bai yi shi yi');
         $this->convert(3456)->shouldBe('san qian si bai wu shi liu');
